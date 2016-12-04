73b994ca4237dfd8fcbdc3345e5bc2ba3990dbff
dajiangliu <dajiang.liu@outlook.com>
2016-12-04 13:31:25 -0500

    message

--- rareMETALS/R/QC.R
+++ rareMETALS/R/QC.R
@@ -568,7 +568,8 @@ imputeConditional <- function(ustat.list,vstat.list,cov.mat.list,N.mat,beta.vec=
                 nSample.covG[kk,jj] <- nSample.covG[jj,kk];
                 print(c('nSample.covG',ii,jj,kk,rm.na(N.mat[ii,jj]),rm.na(N.mat[ii,kk]),sqrt(as.numeric(rm.na(N.mat[ii,jj]))*as.numeric(rm.na(N.mat[ii,kk])))))
                 print(c("rm.na(N.mat[ii,kk])",rm.na(N.mat[ii,kk])));
-                print(nSample.covG[jj,kk]);
+                ##print(nSample.covG[jj,kk]);
+                print(covG[jj,kk]);
             }
         }
     }
