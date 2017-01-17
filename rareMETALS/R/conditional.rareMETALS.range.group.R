98455d5588d5e97616a3adc2160f9a824e899b9e
dajiangliu <dajiang.liu@outlook.com>
2017-01-17 15:55:54 -0500

    message

--- rareMETALS/R/conditional.rareMETALS.range.group.R
+++ rareMETALS/R/conditional.rareMETALS.range.group.R
@@ -235,6 +235,7 @@ conditional.rareMETALS.range.group <- function(range.name=NULL,score.stat.file,c
                   
                   res.impute <- imputeConditional(ustat.list,vstat.list,cov.mat.list,N.mat,NULL,ix.X1,ix.X2);
                   print('res.impute okay');
+                  print(res.impute);
                   res.tmp <- calcGeneLevelTest(res.impute$conditional.ustat,res.impute$conditional.V,maf.vec.rare[ix.X1],as.numeric(res.impute$N.out),test)
                   p.value.out[kk] <- format(res.tmp[[kk]]$p.value,digits=out.digits);
                   statistic.out[kk] <- format(res.tmp[[kk]]$statistic,digits=out.digits);
