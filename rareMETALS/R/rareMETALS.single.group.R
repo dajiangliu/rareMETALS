a4381c8e733af8d6e20a6c63b27d041c0cad5e3b
dajiangliu <dajiang.liu@outlook.com>
2017-03-29 22:03:16 -0400

    message

--- rareMETALS/R/rareMETALS.single.group.R
+++ rareMETALS/R/rareMETALS.single.group.R
@@ -109,6 +109,7 @@ rareMETALS.single.group <- function(score.stat.file,cov.file,range,refaltList,al
                   }
                   res.flipAllele <- flipAllele(raw.data,raw.data.ori,refaltList,ii,ix.var,log.mat[ix.var,],correctFlip,analyzeRefAltListOnly);
                   raw.data <- res.flipAllele$raw.data;
+                  print(raw.data$ustat);
                   ix.include <- res.flipAllele$ix.include;
                   log.mat[ix.var,] <- res.flipAllele$log.mat.var;
                   if(gc==FALSE) 
